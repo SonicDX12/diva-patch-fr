7665521d3ed147c4db3dfb38f0f01f08e17868f1
sonicdx12@gmail.com <sonicdx12@gmail.com@1842fb14-86a6-b6d7-d3e6-64612446997c>
2012-01-13 21:52:32 +0000

    Mise à jour du repo avec l'ajout de la traduction des Pv_start, Objets-Aides pour 2nd# et les écrans lorsqu'on fait <<New Game>> qui concerne l'import de sauvegarde, l'auto-save, etc... sont traduits pour 2nd# et Extend

--- trunk/installer/compile.bat
+++ trunk/installer/compile.bat
@@ -28,12 +28,53 @@ bin2c ..\data\divaext\menu_title_rtmg.png menu_title_rtmg.h menu_title_rtmg
 bin2c ..\data\divaext\menu_title_shop.png menu_title_shop.h menu_title_shop
 bin2c ..\data\divaext\menu_title_visl.png menu_title_visl.h menu_title_visl
 bin2c ..\data\divaext\pv_rank_01.png pv_rank_01.h pv_rank_01
+bin2c ..\data\diva2nd#\pv_start_01.png pv_start_01.h pv_start_01
+bin2c ..\data\diva2nd#\pv_start_04.png pv_start_04.h pv_start_04
+bin2c ..\data\diva2nd#\pv_start_06.png pv_start_06.h pv_start_06
+bin2c ..\data\diva2nd#\pv_start_09.png pv_start_09.h pv_start_09
+bin2c ..\data\diva2nd#\pv_start_11.png pv_start_11.h pv_start_11
+bin2c ..\data\diva2nd#\pv_start_13.png pv_start_13.h pv_start_13
+bin2c ..\data\diva2nd#\pv_start_16.png pv_start_16.h pv_start_16
+bin2c ..\data\diva2nd#\pv_start_17.png pv_start_17.h pv_start_17
+bin2c ..\data\diva2nd#\pv_start_18.png pv_start_18.h pv_start_18
+bin2c ..\data\diva2nd#\pv_start_19.png pv_start_19.h pv_start_19
+bin2c ..\data\diva2nd#\pv_start_20.png pv_start_20.h pv_start_20
+bin2c ..\data\diva2nd#\pv_start_21.png pv_start_21.h pv_start_21
+bin2c ..\data\diva2nd#\pv_start_22.png pv_start_22.h pv_start_22
+bin2c ..\data\diva2nd#\pv_start_23.png pv_start_23.h pv_start_23
+bin2c ..\data\diva2nd#\pv_start_24.png pv_start_24.h pv_start_24
+bin2c ..\data\diva2nd#\pv_start_27.png pv_start_27.h pv_start_27
+bin2c ..\data\diva2nd#\pv_start_36.png pv_start_36.h pv_start_36
 bin2c ..\data\divaext\pv_start_37.png pv_start_37.h pv_start_37
+bin2c ..\data\diva2nd#\pv_start_38.png pv_start_38.h pv_start_38
 bin2c ..\data\divaext\pv_start_39.png pv_start_39.h pv_start_39
 bin2c ..\data\divaext\pv_start_40.png pv_start_40.h pv_start_40
 bin2c ..\data\divaext\pv_start_41.png pv_start_41.h pv_start_41
+bin2c ..\data\diva2nd#\pv_start_42.png pv_start_42.h pv_start_42
+bin2c ..\data\diva2nd#\pv_start_43.png pv_start_43.h pv_start_43
+bin2c ..\data\diva2nd#\pv_start_44.png pv_start_44.h pv_start_44
+bin2c ..\data\diva2nd#\pv_start_45.png pv_start_45.h pv_start_45
+bin2c ..\data\diva2nd#\pv_start_46.png pv_start_46.h pv_start_46
+bin2c ..\data\diva2nd#\pv_start_47.png pv_start_47.h pv_start_47
+bin2c ..\data\diva2nd#\pv_start_48.png pv_start_48.h pv_start_48
+bin2c ..\data\diva2nd#\pv_start_49.png pv_start_49.h pv_start_49
+bin2c ..\data\diva2nd#\pv_start_50.png pv_start_50.h pv_start_50
+bin2c ..\data\diva2nd#\pv_start_51.png pv_start_51.h pv_start_51
+bin2c ..\data\diva2nd#\pv_start_52.png pv_start_52.h pv_start_52
 bin2c ..\data\divaext\pv_start_53.png pv_start_53.h pv_start_53
+bin2c ..\data\diva2nd#\pv_start_54.png pv_start_54.h pv_start_54
+bin2c ..\data\diva2nd#\pv_start_55.png pv_start_55.h pv_start_55
+bin2c ..\data\diva2nd#\pv_start_56.png pv_start_56.h pv_start_56
+bin2c ..\data\diva2nd#\pv_start_57.png pv_start_57.h pv_start_57
+bin2c ..\data\diva2nd#\pv_start_58.png pv_start_58.h pv_start_58
+bin2c ..\data\diva2nd#\pv_start_59.png pv_start_59.h pv_start_59
+bin2c ..\data\diva2nd#\pv_start_60.png pv_start_60.h pv_start_60
+bin2c ..\data\diva2nd#\pv_start_61.png pv_start_61.h pv_start_61
+bin2c ..\data\diva2nd#\pv_start_62.png pv_start_62.h pv_start_62
+bin2c ..\data\diva2nd#\pv_start_63.png pv_start_63.h pv_start_63
+bin2c ..\data\diva2nd#\pv_start_64.png pv_start_64.h pv_start_64
 bin2c ..\data\divaext\pv_start_65.png pv_start_65.h pv_start_65
+bin2c ..\data\diva2nd#\pv_start_66.png pv_start_66.h pv_start_66
 bin2c ..\data\divaext\pv_start_67.png pv_start_67.h pv_start_67
 bin2c ..\data\divaext\pv_start_68.png pv_start_68.h pv_start_68
 bin2c ..\data\divaext\pv_start_69.png pv_start_69.h pv_start_69
@@ -111,7 +152,7 @@ bin2c ..\data\divaext\plst_base_01.png plst_base_01.h plst_base_01
 bin2c ..\data\diva2nd#\menu_result.png menu_result2nd0.h menu_result2nd0
 
 C:/pspsdk/bin/make 
-rm *.prx *.o *.elf *.sfo divapatch5XX.h divapatch6XX.h divaext_translation.h diva2nd0_translation.h divaext_images.h divaext_embedded.h font12dot_01.h menu_help_01.h menu_help_02.h menu_title_adhk.h menu_title_crsl.h menu_title_edit.h menu_title_home.h menu_title_modl.h menu_title_msbx.h menu_title_optn.h menu_title_plys.h menu_title_prvd.h menu_title_room.h menu_title_rtmg.h menu_title_shop.h menu_title_visl.h pv_rank_01.h pv_start_37.h pv_start_39.h pv_start_40.h pv_start_41.h pv_start_53.h pv_start_65.h pv_start_67.h pv_start_68.h pv_start_69.h pv_start_70.h pv_start_71.h pv_start_72.h pv_start_73.h pv_start_74.h pv_start_75.h pv_start_76.h pv_start_77.h pv_start_78.h pv_start_79.h pv_start_80.h pv_start_81.h pv_start_82.h pv_start_83.h pv_start_84.h pv_start_85.h pv_start_86.h pv_start_87.h pv_start_88.h pv_start_89.h pv_start_90.h pv_start_91.h pv_start_92.h pv_start_93.h pv_start_94.h pv_start_95.h pv_start_96.h pv_start_97.h pv_start_edit.h load_00.h load_01.h load_02.h load_03.h load_04.h load_05.h load_06.h load_07.h load_08.h load_09.h load_10.h load_11.h load_12.h load_13.h load_14.h load_15.h load_16.h load_17.h load_18.h load_19.h load_20.h load_21.h load_22.h load_23.h load_24.h load_25.h load_26.h load_27.h load_28.h load_29.h load_30.h load_31.h edit_base_04.h home_base_02.h menu_base_01.h menu_result.h mtit_base_01.h mtit_base_02.h mtit_base_03.h mtit_base_04.h mtit_base_05.h plst_base_01.h diva2nd0_images.h menu_result2nd0.h
+rm *.prx *.o *.elf *.sfo divapatch5XX.h divapatch6XX.h divaext_translation.h diva2nd0_translation.h divaext_images.h divaext_embedded.h font12dot_01.h menu_help_01.h menu_help_02.h menu_title_adhk.h menu_title_crsl.h menu_title_edit.h menu_title_home.h menu_title_modl.h menu_title_msbx.h menu_title_optn.h menu_title_plys.h menu_title_prvd.h menu_title_room.h menu_title_rtmg.h menu_title_shop.h menu_title_visl.h pv_rank_01.h pv_start_01.h pv_start_04.h pv_start_06.h pv_start_09.h pv_start_11.h pv_start_13.h pv_start_16.h pv_start_17.h pv_start_18.h pv_start_19.h pv_start_20.h pv_start_21.h pv_start_22.h pv_start_23.h pv_start_24.h pv_start_27.h pv_start_36.h pv_start_37.h pv_start_38.h pv_start_39.h pv_start_40.h pv_start_41.h pv_start_42.h pv_start_43.h pv_start_44.h pv_start_45.h pv_start_46.h pv_start_47.h pv_start_48.h pv_start_49.h pv_start_50.h pv_start_51.h pv_start_52.h pv_start_53.h pv_start_54.h pv_start_55.h pv_start_56.h pv_start_57.h pv_start_58.h pv_start_59.h pv_start_60.h pv_start_61.h pv_start_62.h pv_start_63.h pv_start_64.h pv_start_65.h pv_start_66.h pv_start_67.h pv_start_68.h pv_start_69.h pv_start_70.h pv_start_71.h pv_start_72.h pv_start_73.h pv_start_74.h pv_start_75.h pv_start_76.h pv_start_77.h pv_start_78.h pv_start_79.h pv_start_80.h pv_start_81.h pv_start_82.h pv_start_83.h pv_start_84.h pv_start_85.h pv_start_86.h pv_start_87.h pv_start_88.h pv_start_89.h pv_start_90.h pv_start_91.h pv_start_92.h pv_start_93.h pv_start_94.h pv_start_95.h pv_start_96.h pv_start_97.h pv_start_edit.h load_00.h load_01.h load_02.h load_03.h load_04.h load_05.h load_06.h load_07.h load_08.h load_09.h load_10.h load_11.h load_12.h load_13.h load_14.h load_15.h load_16.h load_17.h load_18.h load_19.h load_20.h load_21.h load_22.h load_23.h load_24.h load_25.h load_26.h load_27.h load_28.h load_29.h load_30.h load_31.h edit_base_04.h home_base_02.h menu_base_01.h menu_result.h mtit_base_01.h mtit_base_02.h mtit_base_03.h mtit_base_04.h mtit_base_05.h plst_base_01.h diva2nd0_images.h menu_result2nd0.h
 cd vlf
 rm *.h
 cd ..
